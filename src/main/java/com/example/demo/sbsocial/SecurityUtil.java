057db74c038dd11d31443891b139e4d30d87fe62
Tên bạnlucle <lucle99994@gmail.com>
2021-01-13 00:52:29 +0700

    first commit

---
+++ src/main/java/com/example/demo/sbsocial/SecurityUtil.java
@@ -0,0 +1,23 @@
+package com.example.demo.sbsocial;
+
+import java.util.List;
+
+import com.example.demo.model.AppUser;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.social.security.SocialUserDetails;
+
+
+public class SecurityUtil {
+
+
+    public static void logInUser(AppUser user, List<String> roleNames) {
+
+        SocialUserDetails userDetails = new SocialUserDetailsImpl(user, roleNames);
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null,
+                userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+}
